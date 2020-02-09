99a6db2c05f74826477a6a81a32fcb3b40312251
mwulfest <60831981+mwulfest@users.noreply.github.com>
2020-02-08 19:35:06 -0800

    Added Commandline reader

--- Greetings.java
+++ Greetings.java
@@ -2,9 +2,19 @@ import java.io.*;
 
 public class Greetings {
 
-	private static String myName = "Mike\n";
+//	private static String myName = "Mike\n";
 
 	public static void main ( String[] theArgs ) {
-		System.out.println(myName);
+		BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+		String name;
+		try {
+			name = br.readLine();
+			System.out.println(name);
+		} 
+		catch (IOException e) {
+			System.out.println("Error Meassage Goes Here");
+		}
+//		System.out.println(myName);
+		
 	}
 }
