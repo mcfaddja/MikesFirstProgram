c440ee75135ed92cc9c43f7ba1c3a1d06aa5cc83
Jonathan McFadden <mcfaddja@uw.edu>
2020-02-08 17:52:06 -0800

    working base code

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,10 @@
+import java.io.*;
+
 public class Greetings {
+
+	private static String myName = "Mike";
+
 	public static void main ( String[] theArgs ) {
-		
+		System.out.println(myName);
 	}
 }
