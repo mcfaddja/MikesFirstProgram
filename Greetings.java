d2260f8f04df158468fa34bfc966877b0c70dcc7
Jonathan McFadden <mcfaddja@uw.edu>
2020-02-08 18:19:13 -0800

    added a return line

--- Greetings.java
+++ Greetings.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Greetings {
 
-	private static String myName = "Mike";
+	private static String myName = "Mike\n";
 
 	public static void main ( String[] theArgs ) {
 		System.out.println(myName);
